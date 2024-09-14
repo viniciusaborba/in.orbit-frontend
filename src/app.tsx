30b075bcba635483762093fb0f14eb2ee2fdc123
Vinícius Alves Borba <viniboal28@gmail.com>
2024-09-13 21:08:05 -0300

    feat: add inital landing page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,24 @@
-import "./App.css";
+import logoInOrbit from "./assets/logo-in-orbit.svg";
+import letsStart from "./assets/lets-start-illustration.svg";
+import { Plus } from "lucide-react";
 
 export function App() {
-  return <div>hello world</div>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logoInOrbit} alt="logo-in-orbit" />
+      <img src={letsStart} alt="in.orbit" />
+
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  );
 }
