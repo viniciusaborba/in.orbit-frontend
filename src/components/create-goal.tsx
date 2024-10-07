e598a8228c3a21c6c72f33ae9af4212c3e76b650
Vinícius Alves Borba <viniboal28@gmail.com>
2024-10-06 21:55:52 -0300

    feat: add create goal functionality

--- src/components/create-goal.tsx
+++ src/components/create-goal.tsx
@@ -4,13 +4,48 @@ import {
   DialogDescription,
   DialogTitle,
 } from "./ui/dialog";
-import { RadioGroupComponent } from "./radio-group-component";
+import { z } from "zod";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { Label } from "./ui/label";
 import { X } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "./ui/label";
+import {
+  RadioGroup,
+  RadioGroupIndicator,
+  RadioGroupItem,
+} from "./ui/radio-group";
+import { createGoal } from "../http/create-goal";
+import { useQueryClient } from "@tanstack/react-query";
+
+const createGoalForm = z.object({
+  title: z.string().min(1, "Informe a atividade que deseja realizar"),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+});
+
+type createGoalForm = z.infer<typeof createGoalForm>;
 
 export function CreateGoal() {
+  const queryClient = useQueryClient();
+
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<createGoalForm>({
+      resolver: zodResolver(createGoalForm),
+    });
+
+  async function handleCreateGoal(data: createGoalForm) {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    });
+
+    queryClient.invalidateQueries({ queryKey: ["summary"] });
+    queryClient.invalidateQueries({ queryKey: ["pending-goals"] });
+
+    reset();
+  }
+
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
@@ -28,7 +63,11 @@ export function CreateGoal() {
           </DialogDescription>
         </div>
 
-        <form action="" className="flex-1 flex flex-col justify-between">
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          action=""
+          className="flex-1 flex flex-col justify-between"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual a atividade?</Label>
@@ -36,9 +75,92 @@ export function CreateGoal() {
                 id="title"
                 placeholder="Praticar exercícios, meditar, etc..."
                 autoFocus
+                {...register("title")}
               />
+
+              {formState.errors.title && (
+                <p className="text-red-400 text-sm">
+                  {formState.errors.title.message}
+                </p>
+              )}
             </div>
-            <RadioGroupComponent />
+            <Label htmlFor="title">Quantas vezes na semana?</Label>
+            <Controller
+              control={control}
+              name="desiredWeeklyFrequency"
+              defaultValue={1}
+              render={({ field }) => {
+                return (
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    value={String(field.value)}
+                  >
+                    <RadioGroupItem value="1">
+                      <RadioGroupIndicator />
+                      <span className="text-zinc-600 text-sm font-md leading-none">
+                        {" "}
+                        1x na semana
+                      </span>
+                      <span className="font-lg leading-none">🥱</span>
+                    </RadioGroupItem>
+
+                    <RadioGroupItem value="2">
+                      <RadioGroupIndicator />
+                      <span className="text-zinc-600 text-sm font-md leading-none">
+                        {" "}
+                        2x na semana
+                      </span>
+                      <span className="font-lg leading-none">😐</span>
+                    </RadioGroupItem>
+
+                    <RadioGroupItem value="3">
+                      <RadioGroupIndicator />
+                      <span className="text-zinc-600 text-sm font-md leading-none">
+                        {" "}
+                        3x na semana
+                      </span>
+                      <span className="font-lg leading-none">😎</span>
+                    </RadioGroupItem>
+
+                    <RadioGroupItem value="4">
+                      <RadioGroupIndicator />
+                      <span className="text-zinc-600 text-sm font-md leading-none">
+                        {" "}
+                        4x na semana
+                      </span>
+                      <span className="font-lg leading-none">😜</span>
+                    </RadioGroupItem>
+
+                    <RadioGroupItem value="5">
+                      <RadioGroupIndicator />
+                      <span className="text-zinc-600 text-sm font-md leading-none">
+                        {" "}
+                        5x na semana
+                      </span>
+                      <span className="font-lg leading-none">😯</span>
+                    </RadioGroupItem>
+
+                    <RadioGroupItem value="6">
+                      <RadioGroupIndicator />
+                      <span className="text-zinc-600 text-sm font-md leading-none">
+                        {" "}
+                        6x na semana
+                      </span>
+                      <span className="font-lg leading-none">🤯</span>
+                    </RadioGroupItem>
+
+                    <RadioGroupItem value="7">
+                      <RadioGroupIndicator />
+                      <span className="text-zinc-600 text-sm font-md leading-none">
+                        {" "}
+                        Todos os dias da semana
+                      </span>
+                      <span className="font-lg leading-none">🔥</span>
+                    </RadioGroupItem>
+                  </RadioGroup>
+                );
+              }}
+            />
           </div>
 
           <div className="flex items-center gap-3">
